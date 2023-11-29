ef96b7397f4e15eaa5b963c0a8ff5f1fe25488ef
gustavpy <147628928+gustavpy@users.noreply.github.com>
2023-11-29 10:03:40 +0100

    Uppdatering av densitymap

--- metadata_visualisation.py
+++ metadata_visualisation.py
@@ -4,7 +4,9 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 
-df = pd.read_csv("metadata.csv")
+
+df = pd.read_csv("FLEET LEARNING/metadata.csv")
+
 
 app = dash.Dash(__name__)
 
@@ -48,7 +50,7 @@ app.layout = html.Div([
         dcc.Graph(id='collection-car-pie-chart', figure=figs['collection_car']),
     ], style={'display': 'flex'}),
 
-    dcc.Graph(id='scatter-map',figure=densitymap),])
+    dcc.Graph(id='density-map',figure=densitymap),])
 
 
 #Callback för varje pie chart
@@ -58,13 +60,15 @@ app.layout = html.Div([
      Output('scraped-weather-pie-chart', 'figure'),
      Output('time-of-day-pie-chart', 'figure'),
      Output('road-type-pie-chart', 'figure'),
-     Output('collection-car-pie-chart', 'figure')],
+     Output('collection-car-pie-chart', 'figure'),
+     Output('density-map','figure')],
     [Input('country-code-pie-chart', 'clickData'),
      Input('road-condition-pie-chart', 'clickData'),
      Input('scraped-weather-pie-chart', 'clickData'),
      Input('time-of-day-pie-chart', 'clickData'),
      Input('road-type-pie-chart', 'clickData'),
      Input('collection-car-pie-chart', 'clickData')],)
+
 
 #Funktion för att uppdatera pie chartsen när man klickar på någon av de.
 def update_pie_charts(clickData_country_code, clickData_road_condition, clickData_scraped_weather, clickData_time_of_day, clickData_road_type,clickData_collection_car):
@@ -77,6 +81,8 @@ def update_pie_charts(clickData_country_code, clickData_road_condition, clickDat
             updated_fig = px.pie(filtered_df, names=column, title=f'{column} distribution for {selected}') #Uppdaterar pie chartsen
             updated_figs[column] = updated_fig
 
+        densitymap.update_traces(lat=filtered_df["latitude"],lon=filtered_df["longitude"])
+        
     
     elif clickData_road_condition is not None:
         selected = clickData_road_condition['points'][0]['label'] 
@@ -84,20 +90,29 @@ def update_pie_charts(clickData_country_code, clickData_road_condition, clickDat
         for column in df.columns:
             updated_fig = px.pie(filtered_df, names=column, title=f'{column} distribution for {selected}')
             updated_figs[column] = updated_fig
-    
+
+        densitymap.update_traces(lat=filtered_df["latitude"],lon=filtered_df["longitude"])
+        
+        
     elif clickData_scraped_weather is not None:
         selected = clickData_scraped_weather['points'][0]['label'] 
         filtered_df = df[df['scraped_weather'] == selected]
         for column in df.columns: 
             updated_fig = px.pie(filtered_df, names=column, title=f'{column} distribution for {selected}')
             updated_figs[column] = updated_fig
-    
+
+        densitymap.update_traces(lat=filtered_df["latitude"],lon=filtered_df["longitude"])
+        
+
     elif clickData_time_of_day is not None:
         selected = clickData_time_of_day['points'][0]['label']
         filtered_df = df[df['time_of_day'] == selected] 
         for column in df.columns: 
             updated_fig = px.pie(filtered_df, names=column, title=f'{column} distribution for {selected}')
             updated_figs[column] = updated_fig
+        
+        densitymap.update_traces(lat=filtered_df["latitude"],lon=filtered_df["longitude"])
+        
     
     elif clickData_road_type is not None:
         selected = clickData_road_type['points'][0]['label']
@@ -105,6 +120,9 @@ def update_pie_charts(clickData_country_code, clickData_road_condition, clickDat
         for column in df.columns:
             updated_fig = px.pie(filtered_df, names=column, title=f'{column} distribution for {selected}')
             updated_figs[column] = updated_fig
+        
+        densitymap.update_traces(lat=filtered_df["latitude"],lon=filtered_df["longitude"])
+
 
     elif clickData_collection_car is not None:
         selected = clickData_collection_car['points'][0]['label']
@@ -112,13 +130,19 @@ def update_pie_charts(clickData_country_code, clickData_road_condition, clickDat
         for column in df.columns:
             updated_fig = px.pie(filtered_df, names=column, title=f'{column} distribution for {selected}')
             updated_figs[column] = updated_fig
+        
+        densitymap.update_traces(lat=filtered_df["latitude"], lon=filtered_df["longitude"])
+        
 
     else: #Om ingen klickats
         for column in df.columns:
             updated_figs[column] = figs[column]
+
+        densitymap.update_traces(lat=df["latitude"], lon=df["longitude"])
     
-    #Returnerar oavsett om någon klickats eller inte
-    return updated_figs['country_code'], updated_figs['road_condition'], updated_figs['scraped_weather'], updated_figs['time_of_day'], updated_figs['road_type'],updated_figs['collection_car']
+    
+    updated_figs['density_map'] = densitymap
+    return updated_figs['country_code'], updated_figs['road_condition'], updated_figs['scraped_weather'], updated_figs['time_of_day'], updated_figs['road_type'], updated_figs['collection_car'], updated_figs['density_map']
 
 if __name__ == '__main__':
-    app.run_server(debug=True)  
+    app.run_server(debug=True)  #Lägg till så man kan klicka flera 
